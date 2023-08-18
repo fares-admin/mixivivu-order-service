7771c9be6fb5a30b5bf17ab5bbe66b5fa88bc81c
ericchentch <ericchen.tch@gmail.com>
2023-08-18 14:18:02 +0700

    update order authen

--- src/pages/api/orders/update-order.ts
+++ src/pages/api/orders/update-order.ts
@@ -1,6 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
-import { InternalAuthService } from '@/src/service/internal-auth-service/internal-auth-service'
 import { OrderService } from '@/src/service/order-service/order-service'
 import { wrapperEndpoint } from 'common-abstract-fares-system'
 
@@ -12,11 +11,6 @@ import { wrapperEndpoint } from 'common-abstract-fares-system'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const service = new OrderService()
-  const internalService = new InternalAuthService()
-  const authResult = await internalService.authUserToken(req.headers.authorization || '')
-  if (!authResult.success) {
-    res.status(200).json(authResult)
-  }
   const result = await wrapperEndpoint(
     req,
     'GET',
